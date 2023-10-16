535bfdb51b58bb28d7e3f3bc875fc0bb68cef92f
BSuryakumari <suryakumari862@gmail.com>
2023-10-16 12:03:40 +0000

    solution

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -47,50 +47,50 @@ class Login extends Component {
     const {errorMessage} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken) {
-      return <Redirect to="/" />
+      return <Redirect to='/' />
     }
     return (
-      <div className="login-container">
-        <div className="login-form-container">
+      <div className='login-container'>
+        <div className='login-form-container'>
           <img
-            className="logo-img-large"
-            alt="website login"
-            src="https://res.cloudinary.com/srk999/image/upload/v1656568486/desktop/Layer_2_vsdbuc.svg"
+            className='logo-img-large'
+            alt='website login'
+            src='https://res.cloudinary.com/srk999/image/upload/v1656568486/desktop/Layer_2_vsdbuc.svg'
           />
-          <form className="login-form" onSubmit={this.onSubmitForm}>
-            <div className="site-logo-name-container">
+          <form className='login-form' onSubmit={this.onSubmitForm}>
+            <div className='site-logo-name-container'>
               <img
-                className="website-logo"
-                alt="website logo"
-                src="https://res.cloudinary.com/srk999/image/upload/v1656562362/mobile/Standard_Collection_8_a0b8zx.svg"
+                className='website-logo'
+                alt='website logo'
+                src='https://res.cloudinary.com/srk999/image/upload/v1656562362/mobile/Standard_Collection_8_a0b8zx.svg'
               />
-              <h1 className="website-name">Insta Share</h1>
+              <h1 className='website-name'>Insta Share</h1>
             </div>
-            <div className="input-container">
-              <label className="label-text" htmlFor="username">
+            <div className='input-container'>
+              <label className='label-text' htmlFor='username'>
                 USERNAME
               </label>
 
               <input
-                className="user-input"
-                id="username"
-                type="text"
+                className='user-input'
+                id='username'
+                type='text'
                 onChange={this.onChangeUsername}
               />
             </div>
-            <div className="input-container">
-              <label className="label-text" htmlFor="password">
+            <div className='input-container'>
+              <label className='label-text' htmlFor='password'>
                 PASSWORD
               </label>
               <input
-                className="user-input"
-                id="password"
-                type="password"
+                className='user-input'
+                id='password'
+                type='password'
                 onChange={this.onChangePassword}
               />
             </div>
-            <p className="error-msg">{errorMessage}</p>
-            <button className="submit-button" type="submit">
+            <p className='error-msg'>{errorMessage}</p>
+            <button className='submit-button' type='submit'>
               Login
             </button>
           </form>
